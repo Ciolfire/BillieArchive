51c4a9a368e6a3ee5c7f147fd4ac2a8eb13dd2be
Ciol <ciolfire@gmail.com>
2022-02-06 21:43:20 +0100

    feat - allow the stylesheet to be switched

--- assets/js/vampire.js
+++ assets/js/vampire.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../scss/app.scss';
+import '../scss/vampire.scss';
 
 // start the Stimulus application
 import '../bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('character_new', './assets/js/character/new.js')
+    .addEntry('vampire', './assets/js/vampire.js')
+    .addEntry('mortal', './assets/js/mortal.js')
+    // .addEntry('character_new', './assets/js/character/new.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
