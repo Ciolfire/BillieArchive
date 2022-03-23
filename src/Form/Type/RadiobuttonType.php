d474c31c59374b25f8285e9e3c2bc5b24e77d503
Ciol <ciolfire@gmail.com>
2022-03-23 13:42:39 +0100

    front - style, colors and radio button

---
+++ src/Form/Type/RadiobuttonType.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class RadiobuttonType extends AbstractType
+{
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    parent::configureOptions($resolver);
+  }
+
+  public function getParent(): ?string
+  {
+    
+    return ChoiceType::class;
+  }
+}
