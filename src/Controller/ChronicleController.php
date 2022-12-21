2983ba0232930522c2f17e0cabf5a17c80322b2e
Ciol <ciolfire@gmail.com>
2022-12-21 02:14:08 +0100

    refactor - note controller

--- src/Controller/ChronicleController.php
+++ src/Controller/ChronicleController.php
@@ -8,8 +8,6 @@ use App\Entity\NoteCategory;
 use App\Entity\User;
 use App\Form\ChronicleType;
 use App\Form\NoteCategoryType;
-use App\Form\NoteType;
-use App\Repository\NoteRepository;
 use App\Repository\UserRepository;
 use App\Service\CharacterService;
 use App\Service\DataService;
@@ -174,92 +172,6 @@ class ChronicleController extends AbstractController
     ]);
   }
 
-  #[Route('/{id<\d+>}/note/new', name: 'chronicle_note_new', methods: ['GET', 'POST'])]
-  public function addNote(Request $request, Chronicle $chronicle): Response
-  {
-    /** @var User $user */
-    $user = $this->getUser();
-    $note = new Note();
-    $note->setUser($user);
-    $note->setChronicle($chronicle);
-    /** @var NoteRepository $repo */
-    $repo = $this->doctrine->getRepository(Note::class);
-    $linkableNotes = $repo->findByLinkable($user, $note);
-    // Set up date based on chronicle date
-    $form = $this->createForm(NoteType::class, $note, [
-      'categories' => $this->dataService->findBy(NoteCategory::class, ['chronicle' => $chronicle, 'user' => $user]),
-      'notes' => [$linkableNotes],
-    ]);
-    $form->handleRequest($request);
-    
-    if ($form->isSubmitted() && $form->isValid()) {
-      $user->addNote($note);
-      $this->dataService->save($note);
-
-      if ($note->getCategory() instanceof NoteCategory) {
-
-        return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => $note->getCategory()->getId()], Response::HTTP_SEE_OTHER);
-      } else {
-
-        return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => null], Response::HTTP_SEE_OTHER);
-      }
-    }
-    return $this->renderForm('notes/form.html.twig', [
-      'note' => $note,
-      'form' => $form,
-    ]);
-  }
-
-  #[Route('/note/{id<\d+>}/edit', name: 'note_edit', methods: ['GET', 'POST'])]
-  public function editNote(Request $request, Note $note): Response
-  {
-    /** @var User $user */
-    $user = $this->getUser();
-    /** @var NoteRepository $repo */
-    $repo = $this->doctrine->getRepository(Note::class);
-    $linkableNotes = $repo->findByLinkable($user, $note);
-    $form = $this->createForm(NoteType::class, $note, [
-      'categories' => $this->dataService->findBy(NoteCategory::class, ['chronicle' => $note->getChronicle(), 'user' => $user]),
-      'notes' => [$linkableNotes],
-    ]);
-    $form->handleRequest($request);
-    
-    if ($form->isSubmitted() && $form->isValid()) {
-      $this->dataService->flush();
-      if ($note->getCategory() instanceof NoteCategory) {
-
-        return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => $note->getCategory()->getId()], Response::HTTP_SEE_OTHER);
-      } else {
-
-        return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => null], Response::HTTP_SEE_OTHER);
-      }
-
-    }
-
-    return $this->renderForm('notes/form.html.twig', [
-      'note' => $note,
-      'form' => $form,
-    ]);
-  }
-
-  #[Route('/note/{id<\d+>}/delete', name: 'note_delete', methods: ['GET', 'DELETE'])]
-  public function deleteNote(Request $request, Note $note): Response
-  {
-    /** @var User $user */
-    $user = $this->getUser();
-    $category = $note->getCategory();
-    /** @var NoteRepository $repo */
-    $this->dataService->remove($note);
-
-    if ($category instanceof NoteCategory) {
-
-      return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => $category->getId()], Response::HTTP_SEE_OTHER);
-    } else {
-
-      return $this->redirectToRoute('chronicle_notes', ['id' => $note->getChronicle()->getId(), 'category' => null], Response::HTTP_SEE_OTHER);
-    }
-  }
-
   #[Route('/{id<\d+>}/note/category/new', name: 'chronicle_note_category_new', methods: ['GET', 'POST'])]
   public function addNoteCategory(Request $request, Chronicle $chronicle): Response
   {
@@ -305,58 +217,12 @@ class ChronicleController extends AbstractController
   }
 
   #[Route('/{id<\d+>}/note/category/{category<\d+>}/delete', name: 'chronicle_note_category_delete', methods: ['GET', 'DELETE'])]
-  public function deleteCategory(Request $request, Chronicle $chronicle, NoteCategory $category): Response
+  public function deleteCategory(Chronicle $chronicle, NoteCategory $category): Response
   {
     /** @var User $user */
     $user = $this->getUser();
     $this->dataService->remove($category);
 
     return $this->redirectToRoute('chronicle_notes', ['id' => $chronicle->getId()], Response::HTTP_SEE_OTHER);
-  }
-
-  #[Route('/{id<\d+>}/notes/{category<\d+>?0}', name: 'chronicle_notes', methods: ['GET'])]
-  public function notes(Chronicle $chronicle, ?NoteCategory $category): Response
-  {
-    /** @var User $user */
-    $user = $this->getUser();
-    $notes = null;
-    $categories = $this->dataService->findBy(NoteCategory::class, ['chronicle' => $chronicle, 'user' => $user]);
-    
-    if ($category instanceof NoteCategory) {
-      $notes = $this->dataService->findBy(Note::class, ['chronicle' => $chronicle, 'user' => $user, 'category' => $category]);
-    } else {
-      $notes = $this->dataService->findBy(Note::class, ['chronicle' => $chronicle, 'user' => $user, 'category' => null]);
-    }
-
-    return $this->renderForm('notes/chronicle/index.html.twig', [
-      'chronicle' => $chronicle,
-      'current' => $category,
-      'categories' => $categories,
-      'notes' => $notes,
-      'type' => $chronicle->getType(),
-    ]);
-  }
-
-  #[Route('/note/{id<\d+>}', name: 'chronicle_note_view', methods: ['GET'])]
-  public function viewNote(Note $note): Response
-  {
-    /** @var User $user */
-    $user = $this->getUser();
-    $notes = null;
-    $categories = $this->dataService->findBy(NoteCategory::class, ['chronicle' => $note->getChronicle(), 'user' => $user]);
-    $category = $note->getCategory();
-
-    if ($category instanceof NoteCategory) {
-      $notes = $this->dataService->findBy(Note::class, ['chronicle' => $note->getChronicle(), 'user' => $user, 'category' => $category]);
-    }
-
-    return $this->renderForm('notes/chronicle/index.html.twig', [
-      'chronicle' => $note->getChronicle(),
-      'current' => $category,
-      'categories' => $categories,
-      'notes' => $notes,
-      'currentNote' => $note,
-      'type' => $note->getChronicle()->getType(),
-    ]);
   }
 }
