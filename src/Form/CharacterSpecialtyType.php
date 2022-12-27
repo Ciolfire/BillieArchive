86663e364610548c4ae024b969cc401c1dd84c62
Ciol <ciolfire@gmail.com>
2022-12-27 18:57:13 +0100

    front - categories in forms

--- src/Form/CharacterSpecialtyType.php
+++ src/Form/CharacterSpecialtyType.php
@@ -7,11 +7,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CharacterSpecialtyType extends AbstractType
 {
+  public $translator;
+
+  public function __construct(TranslatorInterface $translator)
+  {
+      $this->translator = $translator;
+  }
+
+
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
+    $translator = $this->translator;
     $builder
       ->add('name', null, [
         'attr' => [
@@ -19,7 +29,13 @@ class CharacterSpecialtyType extends AbstractType
           'data-action' => 'character--creation#specialtyUpdate'
         ]
       ])
-      ->add('skill', null, ["label" => 'skill.label']);
+      ->add('skill', null, [
+        'required' => true,
+        'label' => 'skill.label',
+        'group_by' => function($choice) use ($translator) {
+          return $translator->trans($choice->getCategory(), [], 'character');
+        },
+      ]);
   }
 
   public function configureOptions(OptionsResolver $resolver): void
--- src/Form/DisciplinePowerType.php
+++ src/Form/DisciplinePowerType.php
@@ -10,12 +10,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Twig\Extra\Markdown\LeagueMarkdown;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DisciplinePowerType extends AbstractType
 {
+  public $translator;
+
+  public function __construct(TranslatorInterface $translator)
+  {
+      $this->translator = $translator;
+  }
+
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
     $converter = new LeagueMarkdown();
+    $translator = $this->translator;
     /** @var DisciplinePower */
     $power = $options['data'];
     
@@ -25,8 +34,20 @@ class DisciplinePowerType extends AbstractType
       ->add('details', CKEditorType::class, ['empty_data' => '', 'data' => $converter->convert($power->getDetails()), 'label' => false])
       ->add('level', null, ['label' => 'level', 'translation_domain' => "app"])
       ->add('discipline', null, ['label' => 'discipline.label'])
-      ->add('attribute', null, ['label' => 'attribute.label', 'translation_domain' => "character"])
-      ->add('skill', null, ['label' => 'skill.label', 'translation_domain' => "character"])
+      ->add('attribute', null, [
+        'label' => 'attribute.label',
+        'translation_domain' => "character",
+        'group_by' => function($choice) use ($translator) {
+          return $translator->trans($choice->getCategory(), [], 'character');
+        },
+      ])
+      ->add('skill', null, [
+        'label' => 'skill.label',
+        'translation_domain' => "character",
+        'group_by' => function($choice) use ($translator) {
+          return $translator->trans($choice->getCategory(), [], 'character');
+        },
+      ])
       ->add('save', SubmitType::class, ['label' => 'save', 'translation_domain' => "app"]);
   }
 
