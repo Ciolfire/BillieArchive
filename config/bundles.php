5efa707990478a5d3d6e7478970670b937ceac0a
Ciol Erialp <ciolfire@gmail.com>
2024-04-09 01:41:51 +0200

    require - stimulus

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
