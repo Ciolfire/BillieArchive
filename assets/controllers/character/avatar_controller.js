6cb36008c7844403647a5803142e12973903efce
Ciol Erialp <ciolfire@gmail.com>
2024-06-23 04:04:44 +0200

    feat - update avatar instantly (+loading)

--- assets/controllers/character/avatar_controller.js
+++ assets/controllers/character/avatar_controller.js
@@ -5,14 +5,10 @@ export default class extends Controller {
   static targets = [
     "form",
     "avatar",
+    "updatable",
   ];
   static values = {
     id: 0,
-    src: "",
-  }
-
-  connect() {
-    this.srcValue = this.avatarTarget.src;
   }
 
   update(event) {
@@ -20,19 +16,27 @@ export default class extends Controller {
     let xhttp = new XMLHttpRequest();
   
     const avatar = this.avatarTarget;
-    avatar.src = this.srcValue;
+    const updatables = this.updatableTargets;
+    let oldSrc = avatar.src;
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == XMLHttpRequest.DONE) {
         if (xhttp.status == 200) {
           //ok
-          var timestamp = new Date().getTime();
-          avatar.src = `${avatar.src}?t=${timestamp}`;
-          console.log(avatar.src);
-        } else if (xhttp.status == 204) {
+          // let newSrc = JSON.parse(xhttp.response);
+          let timestamp = new Date().getTime();
+          let newSrc = `/images/characters/${JSON.parse(xhttp.response)}?t=${timestamp}`;
+          avatar.src = newSrc;
+          updatables.forEach(element => {
+            element.src = newSrc;
+          });
+        } else {
           console.log('avatar not saved');
+          avatar.src = oldSrc;
         }
       }
     };
+    // temporary update of the image while it's loading :)
+    avatar.src = "/images/loading.gif";
     xhttp.open("POST", `/en/character/${this.idValue}/avatar/update`, true);
     xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
     xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
