9ad67c35b94976230fe0793597e64a31acbf6278
Ciol Erialp <ciolfire@gmail.com>
2024-08-19 00:39:30 +0200

    ref - tabs

--- assets/controllers/tabs_controller.js
+++ assets/controllers/tabs_controller.js
@@ -2,19 +2,38 @@ import { Controller } from '@hotwired/stimulus';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-  static targets = ["tab", "page"];
+  static targets = ["tab", "page", "parent"];
   static values = {
-    initial: String
+    initial: String,
+    parent: String
   };
 
   connect() {
-    let hash = this.initialValue;
     if (window.location.hash) {
-      hash = window.location.hash.substring(1);
+      let hash = window.location.hash.substring(1);
+      // Check if the hash is for this tab group
+      if (this.selectPage(hash)) {
+        console.log(hash);
+        document.getElementById(`${hash}Tab`).classList.add("active");
+        // Found the tab, make sure parent tabs are on
+        if (window.location.hash && this.parentValue) {
+          let parent = document.getElementById(`${this.parentValue}Tab`);
+          parent.click();
+        }
+        return;
+      }
     }
-    this.selectPage(hash);
-    this.unselectTab();
-    document.getElementById(`${hash}Tab`).classList.add("active");
+    // Hash not found/not matching, default tab
+    if (this.initialValue) {
+      this.selectPage(this.initialValue);
+      let tab = document.getElementById(`${this.initialValue}Tab`);
+      if (tab) {
+        tab.classList.add("active");
+      } else {
+        console.log(`${tab} not found`);
+      }
+    }
+    // this.unselectTab();
   }
 
   show(event) {
@@ -24,13 +43,17 @@ export default class extends Controller {
   }
 
   selectPage(selected) {
+    let result = false;
     this.pageTargets.forEach(page => {
       if (page.id == selected) {
         page.classList.remove("d-none");
+        result = true;
       } else {
         page.classList.add("d-none");
       }
     });
+
+    return result;
   }
 
   unselectTab() {
