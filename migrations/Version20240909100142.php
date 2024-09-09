72dffb58747fddfb63d2f788b454739bd531c882
Ciol Erialp <ciolfire@gmail.com>
2024-09-10 01:58:22 +0200

    fix - typos

--- migrations/Version20240909100142.php
+++ migrations/Version20240909100142.php
@@ -21,13 +21,11 @@ final class Version20240909100142 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE organization (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, emblem VARCHAR(255) DEFAULT NULL, description LONGTEXT NOT NULL, overview LONGTEXT NOT NULL, members LONGTEXT NOT NULL, philosophy LONGTEXT NOT NULL, observances LONGTEXT NOT NULL, titles LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE derangement DROP type_bck');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE organization');
-        $this->addSql('ALTER TABLE derangement ADD type_bck VARCHAR(255) DEFAULT NULL');
     }
 }
