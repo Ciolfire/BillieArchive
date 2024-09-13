eb67cf306983f0e53d57eb0b2cbed0b7296f2646
Ciol <ciolfire@gmail.com>
2024-09-13 16:01:37 +0200

    fix - cropping ..

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -874,7 +874,9 @@ class CharacterController extends AbstractController
   public function cropAvatar(Request $request, Character $character, CropperInterface $cropper)
   {
     $filename = addslashes($this->getParameter('characters_directory')."/{$character->getAvatar()}");
-    $filename = substr($filename, 0, strpos($filename, "?"));
+    if (substr($filename, 0, strpos($filename, "?"))) {
+      $filename = $filename;
+    }
 
     $crop = $cropper->createCrop($filename);
 
