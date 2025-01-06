1056c59ed74f6c4550e4ddef3219f6d12d3fca05
ciol <ciolfire@gmail.com>
2025-01-06 02:45:20 +0100

    fix - modal for characters in chronicle, direct link

--- assets/controllers/content_controller.js
+++ assets/controllers/content_controller.js
@@ -5,11 +5,18 @@ export default class extends Controller {
   static targets = [
     "modal"
   ];
+  static values = {
+    link: String,
+  }
+
   connect() {
   }
 
   load(event) {
     this.modalTarget.querySelector("#contentModalTitle").innerHTML = event.params.name;
+    this.linkValue = event.params.link;
+
+    console.log(this.linkValue);
 
     window
     .fetch(`${event.params.url}`)
@@ -17,20 +24,9 @@ export default class extends Controller {
     .then((text) => {
       this.modalTarget.querySelector("#contentModalContainer").innerHTML = text;
     });
-      // .fetch(`${event.params.url}`, {
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     'X-Requested-With': 'XMLHttpRequest'
-      //   }
-      // })
-      // .then((response) => {
-      //   if (!response.ok) {
-      //     throw new Error(`HTTP error! Status: ${response.status}`);
-      //   } 
-      //   return response.blob();
-      // }).then((data) => {
-      //   console.log(data);
-      //   // this.modalTarget.querySelector("#contentModalContainer").innerHTML = response.html;
-      // });
+  }
+
+  open() {
+    window.open(this.linkValue, '_blank').focus();
   }
 }
