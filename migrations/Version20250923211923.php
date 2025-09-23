ee91036234d57514c85ce7fc38ed92511bd85745
ciol <ciolfire@gmail.com>
2025-09-24 00:03:07 +0200

    fix - migrations error

--- migrations/Version20250923211923.php
+++ migrations/Version20250923211923.php
@@ -20,9 +20,9 @@ final class Version20250923211923 extends AbstractMigration
   public function up(Schema $schema): void
   {
     // this up() migration is auto-generated, please modify it to your needs
-    $this->addSql('ALTER TABLE chronicle ADD is_ancient TINYINT(1) DEFAULT 0');
-    $this->addSql('ALTER TABLE characters CHANGE is_ancient is_ancient TINYINT(1) DEFAULT 0');
-    $this->addSql('ALTER TABLE skill CHANGE is_ancient is_ancient TINYINT(1) DEFAULT 0');
+    // $this->addSql('ALTER TABLE chronicle ADD is_ancient TINYINT(1) DEFAULT 0');
+    $this->addSql('ALTER TABLE characters ADD is_ancient is_ancient TINYINT(1) DEFAULT 0');
+    $this->addSql('ALTER TABLE skill ADD is_ancient is_ancient TINYINT(1) DEFAULT 0');
     $this->addSql('ALTER TABLE merits ADD is_ancient TINYINT(1) DEFAULT 0');
     $this->addSql('ALTER TABLE devotion ADD is_ancient TINYINT(1) DEFAULT 0');
     $this->addSql('ALTER TABLE organization ADD is_ancient TINYINT(1) DEFAULT 0');
