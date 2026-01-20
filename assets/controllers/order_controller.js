39c2b9b52ed7aa1f2eadd358973d5c14005ec7b7
ciol <ciolfire@gmail.com>
2026-01-20 11:27:21 +0100

    feat - order npc by id

--- assets/controllers/order_controller.js
+++ assets/controllers/order_controller.js
@@ -10,6 +10,11 @@ export default class extends Controller {
   }
 
   connect() {
+    this.byName();
+    this.containerTarget.classList.remove("collapse");
+  }
+
+  byName() {
     let ordered = this.itemTargets.sort(function (a, b) {
       if (a.dataset.name == "" && b.dataset.name != "") {
         return 1;
@@ -20,6 +25,21 @@ export default class extends Controller {
     });
     this.containerTarget.innerHTML = null;
     ordered.forEach((element) => this.containerTarget.appendChild(element));
-    this.containerTarget.classList.remove("collapse");
+  }
+
+  byId() {
+    let ordered = this.itemTargets.sort(function (a, b) {
+      return a.dataset.id - b.dataset.id;
+    });
+    this.containerTarget.innerHTML = null;
+    ordered.forEach((element) => this.containerTarget.appendChild(element));
+  }
+
+  byIdInv() {
+    let ordered = this.itemTargets.sort(function (a, b) {
+      return b.dataset.id - a.dataset.id;
+    });
+    this.containerTarget.innerHTML = null;
+    ordered.forEach((element) => this.containerTarget.appendChild(element));
   }
 }
