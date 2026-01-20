87f9d7c618f145041929f39302a904486561f5f8
ciol <ciolfire@gmail.com>
2026-01-20 17:26:01 +0100

    package - up stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
+import './stimulus_bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
-import './bootstrap.js';
 import './form.js';
 // start the rabbit application
 import './wrabbit.js';
